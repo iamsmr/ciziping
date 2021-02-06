aa8665d3f6d11f9bbe3ef7206167f136c5267f7d
iamsmr <i.amsameer2061@gmail.com>
2021-02-06 09:50:18 +0545

    code refactor

--- lib/screens/landing/landing.dart
+++ lib/screens/landing/landing.dart
@@ -1,8 +1,7 @@
-import 'package:ciziping/constant/color.dart';
+import 'package:ciziping/screens/landing/landing_details.dart';
 import 'package:ciziping/widgets/centered_view.dart/centered_view.dart';
 import 'package:ciziping/widgets/navigaton_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Landing extends StatefulWidget {
   @override
@@ -17,77 +16,12 @@ class _LandingState extends State<Landing> {
         children: [
           CenteredView(
             child: NavigationBar(
-              items: [
-                NavItem(
-                  title: "Login",
-                  onPressed: () {},
-                )
-              ],
+              items: [NavItem(title: "Login", onPressed: () {})],
             ),
           ),
           Divider(thickness: 1, color: Color(0xffEBEBEB)),
           SizedBox(height: 50),
-          CenteredView(
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: SvgPicture.asset(
-                    "assets/images/main.svg",
-                    height: 350,
-                  ),
-                ),
-                SizedBox(width: 100),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Create Note and store",
-                          style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        SizedBox(height: 50),
-                        Text(
-                          "If you’re looking for a cross-platform"
-                          "note-taking tool with just\nenough frills,"
-                          " it’s hard to look beyond Simplenote.",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.grey[700],
-                            height: 1.7,
-                          ),
-                        ),
-                        SizedBox(height: 50),
-                        SizedBox(
-                          height: 60,
-                          width: 170,
-                          child: FlatButton(
-                            onPressed: () {},
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            color: ColorPalette.primaryColor,
-                            child: Text(
-                              "Get Started",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
+          LandingDetails()
         ],
       ),
     );
